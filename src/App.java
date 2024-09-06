50df043cf1a286ffa132da11b470fe73ad41da3f
Kamil <k.mirgasimov@innopolis.university>
2024-09-06 20:09:54 +0300

    begin

--- src/App.java
+++ src/App.java
@@ -1,4 +1,6 @@
+import Token.Token;
 public class App {
+    Token token;
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
---
+++ src/Token/Token.java
@@ -0,0 +1,3 @@
+package Token;
+public enum Token {
+}
