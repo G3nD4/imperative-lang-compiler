ae62fa452e789d66cdbafb1cbe1a9bdb99f4a250
Kamil <k.mirgasimov@innopolis.university>
2024-11-04 00:20:25 +0300

    Expression optimization

--- src/Nodes/expression/Expression.java
+++ src/Nodes/expression/Expression.java
@@ -12,7 +12,6 @@ public abstract class Expression extends Primary {
     public Type returnType;
 
 
-
     public static Expression parse(ParseTree tree, MyLangParser parser) {
         ParseTree expSubtree = tree;
         int depth = 0;
@@ -20,45 +19,51 @@ public abstract class Expression extends Primary {
         while (expSubtree.getChild(0).getChildCount() != 0) {
             expSubtree = expSubtree.getChild(0);
             switch (depth) {
+                // Если у node только 1 ребёнок, то надо уходить вниз (кроме unaryExpression)
+                // В таком случае игнорим этот expression и идём ниже
                 case 0:
-                    expression = LogicalOrExpression.parse(expSubtree, parser);
                     if (expSubtree.getChildCount() > 1) {
-                        return expression;
+                        expression = LogicalOrExpression.parse(expSubtree, parser);
+                        expression.returnType = Type.BOOLEAN;
+                        return LogicalOrExpression.parse(expSubtree, parser);
                     }
-                    expression.returnType = Type.BOOLEAN;
                     break;
                 case 1:
-                    expression = LogicalAndExpression.parse(expSubtree, parser);
                     if (expSubtree.getChildCount() > 1) {
+                        expression = LogicalAndExpression.parse(expSubtree, parser);
+                        expression.returnType = Type.BOOLEAN;
                         return expression;
                     }
-                    expression.returnType = Type.BOOLEAN;
                     break;
                 case 2:
-                    expression = EqualityExpression.parse(expSubtree, parser);
                     if (expSubtree.getChildCount() > 1) {
+                        expression = EqualityExpression.parse(expSubtree, parser);
+                        expression.returnType = Type.BOOLEAN;
                         return expression;
                     }
-                    expression.returnType = Type.BOOLEAN;
                     break;
                 case 3:
-                    expression = RelationalExpression.parse(expSubtree, parser);
                     if (expSubtree.getChildCount() > 1) {
+                        expression = RelationalExpression.parse(expSubtree, parser);
+                        expression.returnType = Type.BOOLEAN;
                         return expression;
                     }
-                    expression.returnType = Type.BOOLEAN;
                     break;
                 case 4:
-                    expression = AdditiveExpression.parse(expSubtree, parser);
                     if (expSubtree.getChildCount() > 1) {
+                        expression = AdditiveExpression.parse(expSubtree, parser);
                         return expression;
                     }
                     break;
                 case 5:
-                    expression = MultiplicativeExpression.parse(expSubtree, parser);
+                    if (expSubtree.getChildCount() > 1) {
+                        expression = MultiplicativeExpression.parse(expSubtree, parser);
+                        return expression;
+                    }
                     break;
                 case 6:
                     expression = UnaryExpression.parse(expSubtree, parser);
+                    break;
             }
             ++depth;
         }
