bff73057639299c8e71442e3b1f9e817b9dfc56c
Kamil <k.mirgasimov@innopolis.university>
2024-11-30 22:13:21 +0300

    try to implement auto Jasmin executor

--- MyProgram.j
+++ MyProgram.j
@@ -5,48 +5,25 @@
     invokespecial java/lang/Object/<init>()V
     return
 .end method
-.method public static sumOfNumbers(I)I
+.method public static rout(II)I
 .limit stack 100
 .limit locals 100
-ldc 0
-istore_1
-ldc 1
-ldc 1
-istore_2
-for_start_0:
-ldc 10
-iload_2
-if_icmpgt for_end_1
-iload_1
-iload_2
-iadd
-istore_1
-iload_2
-ldc 1
-isub
-istore_2
-goto for_start_0
-for_end_1:
-ldc 1
-istore_3
-ldc 1
-istore_4
-while_start_2:
-iload_3
-iload_4
-iand
-ifeq while_end_3
-ldc 1
-istore_3
-goto while_start_2
-while_end_3:
+	iload_1
+	iload_1
+	imul
+	ldc 1
+	iadd
+	istore_0
+	iload_0
+	ireturn
 .end method
 .method public static main([Ljava/lang/String;)V
 .limit stack 100
 .limit locals 100
-ldc 0
-istore_0
-iload_0
-invokestatic SumProgram/sumOfNumbers(I)I
-return
+	ldc 500
+	istore_0
+	iload_0
+	ldc 5
+	invokestatic SumProgram/rout(II)I
+	return
 .end method
--- src/Tests/unit/ProgramTest.java
+++ src/Tests/unit/ProgramTest.java
@@ -12,15 +12,16 @@ import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.junit.Test;
 
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class ProgramTest {
     @Test
-    public void testSimpleProgram() throws IOException {
+    public void testSimpleProgram() throws IOException, InterruptedException {
 //        CharStream charStream = CharStreams.fromFileName(
 //                "/home/adel/Desktop/compilers-project/imperative-lang-compiler/src/Tests/Test_files/Routine.txt");
         CharStream charStream = CharStreams.fromFileName(
-                "C:\\Users\\HUAWEI\\IdeaProjects\\imperative-lang-compiler\\src\\Tests\\Test_files\\ForLoop.txt");
+                "C:\\Users\\HUAWEI\\IdeaProjects\\imperative-lang-compiler\\src\\Tests\\Test_files\\Routine.txt");
 
         MyLangLexer myLangLexer = new MyLangLexer(charStream);
         CommonTokenStream tokenStream = new CommonTokenStream(myLangLexer);
@@ -30,12 +31,36 @@ public class ProgramTest {
 
         InternalNode root = (InternalNode) TreeBuilder.buildTree(context.getChild(0), myLangParser);
 
-        final Program program = (Program)root.data;
+        final Program program = (Program) root.data;
 
         final CodeGenerator generator = new CodeGenerator();
 
         String result = program.generateInstructions(generator);
         result = AddIndents.add(result);
         System.out.println(result);
+
+        // Or any path that you want
+        try (FileWriter writer = new FileWriter("MyProgram.j")) {
+            writer.write(result);
+        }
+
+
+        // Call Jasmin to assemble the .j file into a .class file
+        Process jasminProcess = new ProcessBuilder("java", "-jar", "C:\\Users\\HUAWEI\\Downloads\\jasmin-2.4.jar", "SumProgram.j")
+                .inheritIO() // Ensures Jasmin output is visible
+                .start();
+        int jasminExitCode = jasminProcess.waitFor();
+        if (jasminExitCode != 0) {
+            throw new RuntimeException("Jasmin failed to assemble the .j file.");
+        }
+
+        // Execute the generated MyProgram.class file
+        Process javaProcess = new ProcessBuilder("java", "SumProgram")
+                .inheritIO() // Ensures program output is visible
+                .start();
+        int javaExitCode = javaProcess.waitFor();
+        if (javaExitCode != 0) {
+            throw new RuntimeException("Execution of MyProgram.class failed.");
+        }
     }
 }
